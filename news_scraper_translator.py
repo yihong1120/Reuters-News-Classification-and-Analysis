42a30eda2edf80cec06c2df65977f9ffe6805efe
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-10-25 08:15:49 -0500

    Update news_scraper_translator.py

--- news_scraper_translator.py
+++ news_scraper_translator.py
@@ -1,46 +1,90 @@
 import requests
 from bs4 import BeautifulSoup
 from googletrans import Translator
+from typing import Optional
 
 class NewsScraper:
-    def __init__(self, url):
-        self.url = url
-        self.title = None
-        self.content = None
-        self.soup = None
+    def __init__(self, url: str) -> None:
+        """
+        Initialise the NewsScraper class.
 
-    def fetch_data(self):
+        Args:
+            url (str): URL of the news article to be scraped.
+
+        """
+        self.url = url
+        self.title: Optional[str] = None
+        self.content: Optional[str] = None
+        self.soup: Optional[BeautifulSoup] = None
+
+    def fetch_data(self) -> None:
+        """
+        Fetch the web page content and initialise the BeautifulSoup object.
+
+        """
+        # Fetch the web page using requests
         response = requests.get(self.url)
+        # Parse the page content with BeautifulSoup
         self.soup = BeautifulSoup(response.content, "html.parser")
 
-    def extract_title(self):
+    def extract_title(self) -> Optional[str]:
+        """
+        Extract the title of the news article.
+
+        Returns:
+            Optional[str]: The extracted title if found, otherwise None.
+
+        """
         if self.soup:
             self.title = self.soup.find("title").text.strip()
         return self.title
 
-    def extract_content(self):
+    def extract_content(self) -> Optional[str]:
+        """
+        Extract the main content of the news article.
+
+        Returns:
+            Optional[str]: The extracted content if found, otherwise None.
+
+        """
         if self.soup:
             self.content = self.soup.find("div", {"class": "caas-body"}).text.strip()
         return self.content
 
 class TextTranslator:
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Initialise the TextTranslator class.
+
+        """
         self.translator = Translator()
 
-    def translate(self, text):
+    def translate(self, text: str) -> str:
+        """
+        Translate the provided text into English.
+
+        Args:
+            text (str): The text to be translated.
+
+        Returns:
+            str: The translated text.
+
+        """
         return self.translator.translate(text).text
 
-# Usage
-if __name__ == "__main__"
+if __name__ == "__main__":
+    # Define the URL for scraping
     url = "https://tw.news.yahoo.com/%E5%A4%A9%E6%B0%A3-%E6%B8%85%E6%98%8E-%E4%B8%8B%E9%9B%A8-233008705.html"
+    # Initialise and fetch news details using NewsScraper
     news_scraper = NewsScraper(url)
     news_scraper.fetch_data()
 
     title = news_scraper.extract_title()
     content = news_scraper.extract_content()
-    print("標題：", title)
-    print("內文：", content)
+    print("Title:", title)
+    print("Content:", content)
 
+    # Translate the extracted title into English
     translator = TextTranslator()
     translated_title = translator.translate(title)
     print("Translated title:", translated_title)
